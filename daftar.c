523b4aa6ee21d82eaf0cb2e87429481ff8ad1139
alit789 <56476878+alit789@users.noreply.github.com>
2022-06-10 01:22:50 +0800

    Add files via upload

---
+++ daftar.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    FILE*DataUser;
+
+    if((DataUser = fopen("DataAkun.txt ", " r"))== NULL){
+        printf("Harap Isi Data Terlebih Dahulu\n");
+        system("pause");
+        system("cls");
+    }
+    printf("Daftar Akun Bank Krut\n");
+    printf("Username : ");
+    char Nama[100];
+    DataUser = fopen("DataAkun.txt","w");
+    gets(Nama);
+    fprintf(DataUser,Nama);
+    fclose(DataUser);
+
+    printf("\nData Berhasil Di Buat");
+    printf("\nAnda Bisa Login Sekarang");
+    return 0;
+}
