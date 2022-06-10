05590835bc29667c63bfc996fb578d2b264ddb02
I Komang Widia Pratama <97996571+WidiaP@users.noreply.github.com>
2022-06-10 12:02:43 +0800

    Update transfer.c

--- transfer.c
+++ transfer.c
@@ -1,7 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-float transfer
+float transfer (float saldo);
 printf("\n\n      MASUKKAN NOMINAL MINIMAL Rp 50.000\n\n");
 printf("                   Rp "); scanf("%llu",&nominal);
 printf("\n\n     1. BENAR\n\n                  2.KELUAR\n\n                  3.KEMBALI KE MENU");
